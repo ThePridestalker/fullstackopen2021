ea4224885eb55d18d2108dcf48f29fd3d1335800
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-08-17 00:27:29 +0100

    part 1.7

--- part1/src/index.js
+++ part1/src/index.js
@@ -6,18 +6,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleClick = (buttonType) => () => {
     if (buttonType === 'good') {
       setGood(good + 1)
+      setAll(all + 1)
     }
     if (buttonType === 'neutral') {
       setNeutral(neutral + 1)
+      setAll(all + 1)
     }
     if (buttonType === 'bad') {
       setBad(bad + 1)
+      setAll(all + 1)
     }
   }
+
+  // the average score (good: 1, neutral: 0, bad: -1)
 
   return (
     <div>
@@ -27,9 +33,12 @@ const App = () => {
       <button onClick={handleClick('bad')}>bad</button>
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {all === 0 ? 0 : (good - bad) / all}</div>
+      <div>positive {all === 0 ? 0 : (good * 100) / all} %</div>
     </div>
   )
 }
