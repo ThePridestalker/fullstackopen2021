157a12b914b104e32a43341b33596ec2956cbbf3
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-08-17 00:59:20 +0100

    part 1.8

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,44 +1,38 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-
-  const handleClick = (buttonType) => () => {
-    if (buttonType === 'good') {
-      setGood(good + 1)
-      setAll(all + 1)
-    }
-    if (buttonType === 'neutral') {
-      setNeutral(neutral + 1)
-      setAll(all + 1)
-    }
-    if (buttonType === 'bad') {
-      setBad(bad + 1)
-      setAll(all + 1)
-    }
-  }
-
-  // the average score (good: 1, neutral: 0, bad: -1)
-
+// a proper place to define a component
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={handleClick('good')}>good</button>
-      <button onClick={handleClick('neutral')}>neutral</button>
-      <button onClick={handleClick('bad')}>bad</button>
-
-      <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
       <div>all {all}</div>
       <div>average {all === 0 ? 0 : (good - bad) / all}</div>
       <div>positive {all === 0 ? 0 : (good * 100) / all} %</div>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClick = (value, increaser) => () => increaser(value + 1)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={handleClick(good, setGood)}>good</button>
+      <button onClick={handleClick(neutral, setNeutral)}>neutral</button>
+      <button onClick={handleClick(bad, setBad)}>bad</button>
+
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
