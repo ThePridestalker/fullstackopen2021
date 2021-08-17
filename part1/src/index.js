342e3f67259eea431219771363a9eab417581b8f
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-08-17 01:36:13 +0100

    part 1.11

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,9 +7,10 @@ const Button = ({ onClick, text }) => {
 
 const StatisticLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -19,14 +20,16 @@ const Statistics = ({ good, neutral, bad }) => {
     const average = (good - bad) / all
     const positive = (good * 100) / all + ' %'
     return (
-      <div>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={all} />
-        <StatisticLine text="average" value={average} />
-        <StatisticLine text="positive" value={positive} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={average} />
+          <StatisticLine text="positive" value={positive} />
+        </tbody>
+      </table>
     )
   }
   return <div>No feedback given</div>
