646aa2167f8b97f38b783b3aef89e6bdd0723021
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-08-18 22:35:53 +0100

    part 2.5

---
+++ part2/src/components/Total.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Total = ({ parts }) => {
+  return (
+    <strong>
+      Total of {parts.reduce((acc, part) => acc + part.exercises, 0)} exercises
+    </strong>
+  )
+}
+
+export default Total
