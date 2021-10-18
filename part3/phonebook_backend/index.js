c384d52f562a114b8672b4ab2d8eb2dd5e4575c1
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-10-18 23:21:44 +0100

    part 3.15

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -55,10 +55,12 @@ app.get('/api/persons/:id', (request, response) => {
   }
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter((person) => person.id !== id)
-  response.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id
+
+  Person.findByIdAndRemove(id).then(() => {
+    res.status(204).end()
+  })
 })
 
 app.post('/api/persons', (request, response) => {
