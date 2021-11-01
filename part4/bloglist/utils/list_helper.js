836d79f112f52698411e302ddaeaf646cfb23acd
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-11-01 21:11:23 +0000

    part 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -12,7 +12,15 @@ const totalLikes = (blogs) => {
   return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (mostLiked, blog) => {
+    return (mostLiked.likes > blog.likes) ? mostLiked : blog
+  }
+  return blogs.reduce(reducer)
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
