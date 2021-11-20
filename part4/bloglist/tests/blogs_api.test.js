bb14cbf844dd292e1f660b6996d99485776d7f72
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-11-20 20:12:06 +0000

    part 4.9

--- part4/bloglist/tests/blogs_api.test.js
+++ part4/bloglist/tests/blogs_api.test.js
@@ -23,6 +23,11 @@ test('correct amount of blog posts are returned in JSON format', async () => {
   expect(response.body.length).toBe(helper.initialBlogs.length)
 })
 
+test('the unique identifier property of the blog posts is named id', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
