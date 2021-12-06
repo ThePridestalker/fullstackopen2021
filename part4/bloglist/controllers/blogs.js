2ebbf9405f9bfbba0487d6b5c6230dff1f63299b
ThePridestalker <axel_martin_ramos@hotmail.com>
2021-12-06 21:22:31 +0000

    part 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,25 +1,32 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const users = await User.find({})
+  const randomUser = users[Math.floor(Math.random() * users.length)]
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes || 0
+    likes: body.likes || 0,
+    user: randomUser._id
   })
 
   if (!blog.title || !blog.url) {
     response.status(400).json('title or url is missing')
   } else {
     const savedBlog = await blog.save()
+    randomUser.blogs = randomUser.blogs.concat(savedBlog._id)
+    await randomUser.save()
     response.status(201).json(savedBlog)
   }
 })
