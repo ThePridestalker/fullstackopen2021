a1363166e067446a3cd201091fbe3e260c6c4ec6
ThePridestalker <axel_martin_ramos@hotmail.com>
2022-01-18 23:27:21 +0000

    part 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,25 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+const Blog = ({ blog }) => {
+  const [displayDetails, setDisplayDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} <button onClick={() => setDisplayDetails(!displayDetails)}>{displayDetails ? 'hide' : 'show'}</button> <br />
+      {displayDetails &&
+        <div>{blog.url}<br />
+          likes {blog.likes} <button>like</button> <br />
+          {blog.author}<br />
+        </div>}
+    </div>
+  )
+}
 
 export default Blog
