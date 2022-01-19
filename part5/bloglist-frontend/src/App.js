2ae0df1cd4c3519ac15b10517e5cd1f9125f227c
ThePridestalker <axel_martin_ramos@hotmail.com>
2022-01-19 23:17:23 +0000

    part 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -62,15 +62,19 @@ const App = () => {
     </Togglable>
   )
 
-  const blogList = () => (
-    <div>
-      <p>{user.name} logged in <button onClick={handleLogout}>logout</button> </p>
-      {createNew()}
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} handleUpdateBlog={handleUpdateBlog} />
-      )}
-    </div>
-  )
+  const blogList = () => {
+    // sorted from higher likes to lower likes
+    const sortedBlogs = blogs.sort((a, b) => b.likes - a.likes)
+    return (
+      <div>
+        <p>{user.name} logged in <button onClick={handleLogout}>logout</button> </p>
+        {createNew()}
+        {sortedBlogs.map(blog =>
+          <Blog key={blog.id} blog={blog} handleUpdateBlog={handleUpdateBlog} />
+        )}
+      </div>
+    )
+  }
 
   const handleLogin = async (event) => {
     event.preventDefault()
