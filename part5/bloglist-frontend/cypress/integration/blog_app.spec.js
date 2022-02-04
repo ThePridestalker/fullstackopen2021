4d5de1e73eebae7e16aaa2d9360b0eb7aff8e1db
ThePridestalker <axel_martin_ramos@hotmail.com>
2022-02-04 02:14:25 +0000

    part 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,36 @@
 describe('Blog App', function () {
+  let user
+
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
     cy.visit('http://localhost:3000')
+
+    user = {
+      name: 'Axel',
+      username: 'Axelinho95',
+      password: 'testingpassword'
+    }
+
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
   })
 
   it('the application displays the login form by default', function () {
     cy.get('#login-form')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type(user.username)
+      cy.get('#password').type(user.password)
+      cy.get('#login-button').click()
+      cy.contains(`${user.name} logged in`)
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('wrongUsername')
+      cy.get('#password').type('wrongPassword')
+      cy.get('#login-button').click()
+      cy.contains('wrong username or password').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
