2cb311c3b0fd6a7d03ff31b2812c0130d17fa155
ThePridestalker <axel_martin_ramos@hotmail.com>
2022-02-11 04:38:43 +0000

    part 6.10

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -39,7 +39,7 @@ export const voteAnecdote = (id) => {
   }
 }
 
-const reducer = (state = initialState, action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
@@ -62,4 +62,4 @@ const reducer = (state = initialState, action) => {
   }
 }
 
-export default reducer
+export default anecdoteReducer
---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,8 @@
+const notificationReducer = (state = null, action) => {
+  console.log('NOTIFICATION REDUCER - ACTION', action)
+  console.log('NOTIFICATION REDUCER - STATE', state)
+
+  return state
+}
+
+export default notificationReducer
